fa9ab284a86f439b926de3810ab35a15d003f8b5
AROMAL  M B <130879163+Aromal-m-b@users.noreply.github.com>
2024-07-20 15:41:51 +0530

    Add files via upload

---
+++ Brand_Name_Generator.py
@@ -0,0 +1,3 @@
+location = input("Welcome to Band Name Generator\nWhat's the name of the city you grew up\n")
+pet = input("What is the name of your pet\n")
+print("Your Band Name could be "+location+" "+pet)
