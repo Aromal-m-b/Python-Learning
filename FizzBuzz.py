4b85333a893f6c12a670ac2b3fd5c9de853e5e8e
AROMAL  M B <130879163+Aromal-m-b@users.noreply.github.com>
2024-07-23 22:50:35 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,4 @@
+print("Welcome to my FizzBuzz Game\nin this game we count numbers from 0  to 100 \nFor each multiple of 3 occurs we will say Fizz\nFor each Multiple of 5 we will say Buzz\nFor each mutliple of 3 and 5 we will say FizzBuzz")
 for i in range (0,101):
     if i%3 == 0 and i%5 == 0:
         print("FizzBuzz")
